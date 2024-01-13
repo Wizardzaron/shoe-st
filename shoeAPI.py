0f2f66bf68ca933447af8e872c7cb668883989b2
Wizardzaron <mochi456@csu.fullerton.edu>
2024-01-13 11:51:16 -0800

    changing shoedata endpoint to only retrieve on row of data for testing purposes

--- shoeAPI.py
+++ shoeAPI.py
@@ -101,7 +101,7 @@ def shoedata_get():
     rows = []
     try:
 
-        getInfo =  '''SELECT names, item_id, category, brand, color, gender, shoesize, price ,images, descript FROM shoes'''
+        getInfo =  '''SELECT names, item_id, category, brand, color, gender, shoesize, price ,images, descript FROM shoes WHERE item_id = FB7582-001'''
         cur.execute(getInfo)
         info = cur.fetchall()
 
