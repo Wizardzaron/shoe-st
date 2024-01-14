c2a18b2e4ba7baf6863beada7f31c75bcdbaa484
Wizardzaron <mochi456@csu.fullerton.edu>
2024-01-13 17:00:06 -0800

    reverting the shoedata endpoint back to it's original purpose

--- shoeAPI.py
+++ shoeAPI.py
@@ -101,7 +101,7 @@ def shoedata_get():
     rows = []
     try:
 
-        getInfo =  '''SELECT names, item_id, category, brand, color, gender, shoesize, price ,images, descript FROM shoes WHERE shoesize = 7.3'''
+        getInfo =  '''SELECT names, item_id, category, brand, color, gender, shoesize, price ,images, descript FROM shoes'''
         cur.execute(getInfo)
         info = cur.fetchall()
 
