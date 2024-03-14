abc820e946405583c7c46c3f5a2e890d37706a15
Wizardzaron <mochi456@csu.fullerton.edu>
2024-03-14 15:38:01 -0700

    fixing small problem

--- front-end-web/src/app/checkout/page.jsx
+++ front-end-web/src/app/checkout/page.jsx
@@ -2,7 +2,7 @@
 import React, {useState, useEffect} from 'react';
 import styles from '../page.module.css'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
+//import { useRouter } from 'next/router'
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha512-kgrXVPaJi1gUqEbb3lVdpJ3tWjfCfEUGD4tKm5GpUW1Gy6PIq3n89kk0zEIWxgBlfpaZD7lcmTFdHL8u5eSzug==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 const Checkout = () => {
