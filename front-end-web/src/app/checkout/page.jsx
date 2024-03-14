b58b11acca8d3695482cded7b3d6f7193468089b
Wizardzaron <mochi456@csu.fullerton.edu>
2024-03-14 15:39:17 -0700

    commenting out checkout

--- front-end-web/src/app/checkout/page.jsx
+++ front-end-web/src/app/checkout/page.jsx
@@ -1,20 +1,20 @@
-'use client'
-import React, {useState, useEffect} from 'react';
-import styles from '../page.module.css'
-import Link from 'next/link'
-//import { useRouter } from 'next/router'
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha512-kgrXVPaJi1gUqEbb3lVdpJ3tWjfCfEUGD4tKm5GpUW1Gy6PIq3n89kk0zEIWxgBlfpaZD7lcmTFdHL8u5eSzug==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+// 'use client'
+// import React, {useState, useEffect} from 'react';
+// import styles from '../page.module.css'
+// import Link from 'next/link'
+// //import { useRouter } from 'next/router'
+// <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha512-kgrXVPaJi1gUqEbb3lVdpJ3tWjfCfEUGD4tKm5GpUW1Gy6PIq3n89kk0zEIWxgBlfpaZD7lcmTFdHL8u5eSzug==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-const Checkout = () => {
-
-
-
-    const router = useRouter();
-    const data = router.query;
-    console.log(data);
+// const Checkout = () => {
 
 
 
-}
+//     const router = useRouter();
+//     const data = router.query;
+//     console.log(data);
 
-export default Checkout;
+
+
+// }
+
+// export default Checkout;
